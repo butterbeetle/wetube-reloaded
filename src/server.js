2dff864e35ca9b2aee600bb6c2597b31228f447a
butterbeetle <butterbeetle@naver.com>
2022-05-17 21:35:37 +0900

    ~6.19

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -13,15 +13,9 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const home = (req, res) => {
-    return res.send("hello");
-}
-
-const handleListening = () =>
-    console.log(`Server listening on port http://localhost:${PORT} ✨`);
-
-app.listen(PORT, handleListening);
+export default app;

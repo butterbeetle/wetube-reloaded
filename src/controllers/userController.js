9026c90b3b9474f68dbb4556c7feefa88222a42b
butterbeetle <butterbeetle@naver.com>
2022-05-19 22:09:51 +0900

    7.4 Status Codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,15 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-    const { name, username, email, password, location } = req.body;
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.status(400).res.render("join", { pageTitle, errorMessage: "Password confirmation does not match.", });
+    }
+    const usernameExists = await User.exists({ $or: [{ username }, { email }] });
+    if (usernameExists) {
+        return res.status(400).res.render("join", { pageTitle, errorMessage: "This username or email is already taken.", });
+    }
     await User.create({
         name, username, email, password, location,
     })

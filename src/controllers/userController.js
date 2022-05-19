a4a6eba897a768268ec407e4778e54238c417c8c
butterbeetle <butterbeetle@naver.com>
2022-05-19 22:36:11 +0900

    7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,39 @@
 import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postJoin = async (req, res) => {
     const { name, username, email, password, password2, location } = req.body;
     const pageTitle = "Join";
     if (password !== password2) {
-        return res.status(400).res.render("join", { pageTitle, errorMessage: "Password confirmation does not match.", });
+        return res.status(400).render("join", { pageTitle, errorMessage: "Password confirmation does not match.", });
     }
     const usernameExists = await User.exists({ $or: [{ username }, { email }] });
     if (usernameExists) {
-        return res.status(400).res.render("join", { pageTitle, errorMessage: "This username or email is already taken.", });
+        return res.status(400).render("join", { pageTitle, errorMessage: "This username or email is already taken.", });
     }
-    await User.create({
-        name, username, email, password, location,
-    })
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name, username, email, password, location,
+        })
+        return res.redirect("/login");
+    } catch (error) {
+        console.log(error);
+        return res.status(400).render("join", { pageTitle, errorMessage: error._message, });
+    }
 }
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) =>
+    res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const exists = await User.exists({ username });
+    if (!exists) {
+        return res.status(400).render("login", { pageTitle: "Login", errorMessage: "An account with this username does not exists.", });
+    }
+    res.end();
+}
 export const logout = (req, res) => res.send("Log Out");
+export const remove = (req, res) => res.send("Remove User");
+export const edit = (req, res) => res.send("Edit User");
 export const see = (req, res) => res.send("See User");

218adbaf7286c4d39e273cc4079486bb7eac2877
butterbeetle <butterbeetle@naver.com>
2022-05-26 00:49:33 +0900

    8.2 Edit profile POST part One

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
+import { all } from "express/lib/application";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
+import { protectedMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-g]{24})", watch);
-videoRouter.route("/:id([0-9a-g]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-g]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-g]{24})/edit").all(protectedMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-g]{24})/delete").all(protectedMiddleware).get(deleteVideo);
+videoRouter.route("/upload").all(protectedMiddleware).get(getUpload).post(postUpload);
 
 export default videoRouter;

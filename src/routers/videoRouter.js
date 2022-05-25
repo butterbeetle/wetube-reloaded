24b7f9a79feeed05a19ce8e6c31d92da2498167f
butterbeetle <butterbeetle@naver.com>
2022-05-26 02:46:34 +0900

    8.9 Video Upload

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { all } from "express/lib/application";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
-import { protectedMiddleware } from "../middlewares";
+import { protectedMiddleware, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-g]{24})", watch);
 videoRouter.route("/:id([0-9a-g]{24})/edit").all(protectedMiddleware).get(getEdit).post(postEdit);
 videoRouter.route("/:id([0-9a-g]{24})/delete").all(protectedMiddleware).get(deleteVideo);
-videoRouter.route("/upload").all(protectedMiddleware).get(getUpload).post(postUpload);
+videoRouter.route("/upload").all(protectedMiddleware).get(getUpload).post(videoUpload.single("video"), postUpload);
 
 export default videoRouter;

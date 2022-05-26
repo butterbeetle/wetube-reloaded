66d097c464bb692f93ef907ce87ae001615a73c6
butterbeetle <butterbeetle@naver.com>
2022-05-26 21:25:46 +0900

    8.11 Video Owner

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async (req, res) => {
     const videos = await Video.find({}).sort({ createdAt: "desc" });
@@ -7,10 +8,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    const owner = await User.findById(video.owner);
     if (!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
-    return res.render("watch", { pageTitle: video.title, video });
+    return res.render("watch", { pageTitle: video.title, video, owner });
 };
 export const getEdit = async (req, res) => {
     const { id } = req.params;
@@ -40,6 +42,9 @@ export const getUpload = (req, res) => {
 }
 
 export const postUpload = async (req, res) => {
+    const {
+        user: { _id },
+    } = req.session;
     const { path: fileUrl } = req.file;
     const { title, description, hashtags } = req.body;
     try {
@@ -47,6 +52,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             fileUrl,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
@@ -58,7 +64,7 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
     const { id } = req.params;
-    console.log(id);
+    //console.log(id);
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
 }

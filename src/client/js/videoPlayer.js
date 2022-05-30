e96fd0bccbb969e2d54b40d4000b19def54a2873
butterbeetle <butterbeetle@naver.com>
2022-05-30 23:42:36 +0900

    11.7 Fullscreen

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
-
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -42,10 +42,17 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 }
 
 const handleTimeupdate = () => {
     currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+}
+
+const handleTimeline = (event) => {
+    const { target: { value } } = event;
+    video.currentTime = value;
 }
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -53,3 +60,4 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata)
 video.addEventListener("timeupdate", handleTimeupdate)
+timeline.addEventListener("input", handleTimeline);

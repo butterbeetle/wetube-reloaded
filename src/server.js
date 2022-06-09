f6bdb865d52f8d5d14dc3e771b93e3c14eb0d06c
butterbeetle <butterbeetle@naver.com>
2022-06-09 15:55:59 +0900

    14.1 Transcode Video

--- src/server.js
+++ src/server.js
@@ -27,8 +27,17 @@ app.use(session({
 }))
 
 app.use(localsMiddleware);
+
+// for ffmpeg error -> Uncaught (in promise) ReferenceError: SharedArrayBuffer is not defined
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
+
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
+app.use("/convert", express.static("node_modules/@ffmpeg/core/dist"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);

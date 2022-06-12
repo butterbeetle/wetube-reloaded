e27c9f5fe5090ebcc5801e93e94475a91f90462f
butterbeetle <butterbeetle@naver.com>
2022-06-13 04:11:49 +0900

    bug fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -12,7 +12,8 @@ const s3 = new aws.S3({
 const multerUploader = multerS3({
     s3: s3,
     bucket: "wetube-shoe",
-})
+    acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
